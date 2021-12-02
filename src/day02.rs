27439d9d5ee76fb18f16fe207d248868588d37a0
Kalle Soiha <ksoiha@gmail.com>
2021-12-03 00:25:55 +0200

    Day 2

---
+++ src/day02.rs
@@ -0,0 +1,38 @@
+﻿pub fn day02(input: String) -> String {
+    let v: Vec<&str> = input.lines().collect();
+    let mut pos: i32 = 0;
+    let mut depth: i32 = 0;
+    let mut aim: i32 = 0;
+    let mut pos_aimed: i32 = 0;
+    let mut depth_aimed: i32 = 0;
+    
+    for s in v {
+        let mut pair = s.split_ascii_whitespace();
+        let command = pair.next().unwrap();
+        let amount: i32 = pair.next().unwrap().parse().unwrap();
+        
+        match command {
+            "forward" => {
+                pos += amount;
+                
+                pos_aimed += amount;
+                depth_aimed += aim * amount;
+            },
+
+            "up" => {
+                depth -= amount;
+                aim -= amount;
+            },
+            
+            "down" => {
+                depth += amount;
+                aim += amount;
+            },
+            
+            _ => println!("Unknown command {}", command),
+        }
+    }
+    
+    return format!("\nInitial: Pos {} Depth {} Multiplied {}\nAimed:   Pos {} Depth {} Multiplied {}\n", 
+                   pos, depth, pos * depth, pos_aimed, depth_aimed, pos_aimed * depth_aimed);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
 use std::fs;
 
+#[allow(dead_code)]
+
 mod day01;
+mod day02;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file = &args[1];
     let input = fs::read_to_string(file)
         .expect(format!("Could not read input file {}", file).as_str());
-    let result = crate::day01::day01(input);
+    let result = crate::day02::day02(input);
     
     print!("Result is: {}", result);
 }
